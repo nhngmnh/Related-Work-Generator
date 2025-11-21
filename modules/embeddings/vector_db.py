f5d3616a21517d7074912af4e8ff5a4ea978db7d
nhngmnh <nhungocminh2004@gmail.com>
2025-11-22 01:28:10 +0700

    add id to collection

--- modules/embeddings/vector_db.py
+++ modules/embeddings/vector_db.py
@@ -6,6 +6,7 @@ collection = chroma.get_or_create_collection("papers")
 
 def save_chunk(chunk, embedding, metadata):
     collection.add(
+        ids=[metadata["id"]],
         documents=[chunk],
         embeddings=[embedding],
         metadatas=[metadata]
